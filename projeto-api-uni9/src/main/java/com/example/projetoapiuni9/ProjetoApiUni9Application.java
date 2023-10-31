4c33becd032d95051a36ab539a58e05b6bfb8665
Jonathan <jonathandscoutinho@gmail.com>
2023-10-31 20:49:25 -0300

    initial commit

---
+++ projeto-api-uni9/src/main/java/com/example/projetoapiuni9/ProjetoApiUni9Application.java
@@ -0,0 +1,13 @@
+package com.example.projetoapiuni9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiUni9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiUni9Application.class, args);
+	}
+
+}
---
+++ projeto-api-uni9/src/test/java/com/example/projetoapiuni9/ProjetoApiUni9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoapiuni9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiUni9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
