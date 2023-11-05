2592866b23ee314af603a34934cc0f128abf2017
Jonathan <jonathandscoutinho@gmail.com>
2023-11-05 18:58:36 -0300

    update 05-11

---
+++ projeto-api-uni9/src/main/java/com/example/projetoapiuni9/DAO/IUsuario.java
@@ -0,0 +1,9 @@
+package com.example.projetoapiuni9.DAO;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.projetoapiuni9.model.Usuario;
+
+public interface IUsuario extends CrudRepository<Usuario, Integer>{
+	
+}
